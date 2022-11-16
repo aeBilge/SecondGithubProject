f860d0ce30516e8235ed8df13aa63e8832660f2c
Main PC <alibilge34.28@gmail.com>
2022-11-16 17:40:44 +0100

    This is my first class in the second project

---
+++ src/FirsClass.java
@@ -0,0 +1,15 @@
+public class FirsClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("MyFirstClassInTheSecondProject");
+
+        int[] array = {1,2,3,4,5,6,7,8,9};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + " ");
+        }
+
+    }
+
+}
